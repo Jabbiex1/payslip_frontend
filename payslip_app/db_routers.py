1c8e47e41f1867fe93550bd911655f4cc7caa361
Abdulai Nuru Jabbie <jabbieabdulai582@gmail.com>
2026-03-06 15:09:32 +0000

    Fix database

--- payslip_app/db_routers.py
+++ payslip_app/db_routers.py
@@ -1,21 +1,10 @@
 class PayslipRouter:
-
-    route_app_labels = {"payslip_app"}
-
     def db_for_read(self, model, **hints):
-        if model.__name__ == "EmployeePayslip":
-            return "mock_payslips"
-        return "default"
+        if model._meta.app_label == 'payslip_app':
+            return 'default'
+        return None
 
     def db_for_write(self, model, **hints):
-        if model.__name__ == "EmployeePayslip":
-            return "mock_payslips"
-        return "default"
-
-    def allow_relation(self, obj1, obj2, **hints):
-        return True
-
-    def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if model_name == "employeepayslip":
-            return db == "mock_payslips"
-        return db == "default"
+        if model._meta.app_label == 'payslip_app':
+            return 'default'
+        return None
