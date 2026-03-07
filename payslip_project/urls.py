f0acc6962f387cd129fac6b2b0fbd12bf948ba94
Abdulai Nuru Jabbie <jabbieabdulai582@gmail.com>
2026-03-07 18:11:02 +0000

    urls fix

--- payslip_project/urls.py
+++ payslip_project/urls.py
@@ -5,8 +5,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),  # Default Django admin (superuser)
-    path('', include('payslip_app.urls')),      # Your app URLs
+    path('', include('payslip_app.urls')),  
+        # Your app URLs
 ]
+from django.contrib import admin
 
+urlpatterns = [
+    path("x92k-secret-admin/", admin.site.urls),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
