d9044dc974ae6e2789ed5bd160953eff2b761293
Abdulai Nuru Jabbie <jabbieabdulai582@gmail.com>
2026-03-07 18:57:35 +0000

    Create superuser on deploy

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "DATABASE_URL is: $DATABASE_URL"
-echo "DJANGO_SETTINGS_MODULE is: $DJANGO_SETTINGS_MODULE"
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'ChangeMe123!')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
