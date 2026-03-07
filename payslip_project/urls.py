0cd877376da180abce2a159331777c9904ba1dda
Abdulai Nuru Jabbie <jabbieabdulai582@gmail.com>
2026-03-07 18:18:49 +0000

    url

--- payslip_project/urls.py
+++ payslip_project/urls.py
@@ -8,10 +8,5 @@ urlpatterns = [
     path('', include('payslip_app.urls')),  
         # Your app URLs
 ]
-from django.contrib import admin
-
-urlpatterns = [
-    path("x92k-secret-admin/", admin.site.urls),
-]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
